e0d5898705c54032300d5d726d412a2fddd75610
Akhil Naidu <akhiln1994@gmail.com>
2020-10-26 22:40:57 +0530

    added validations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material/material/material.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { HeaderComponent } from './components/header/header.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule } from "./material/material/material.module";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { HeaderComponent } from "./components/header/header.component";
+import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from "@angular/forms";
+
+export const passwordMatchValidator: ValidatorFn = (
+  control: FormGroup
+): ValidationErrors | null => {
+  const password = control.get("password");
+  const confirmPassword = control.get("confirmPassword");
+
+  const errors =
+    password && confirmPassword && password.value === confirmPassword.value
+      ? null
+      : { passwordMismatch: true };
+  confirmPassword.setErrors(errors);
+  return errors;
+};
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.scss"],
 })
 export class RegisterComponent implements OnInit {
+  constructor(private fb: FormBuilder) {}
 
-  constructor() { }
+  registerForm = this.fb.group(
+    {
+      firstName: ["", [Validators.required, Validators.minLength(3)]],
+      lastName: ["", [Validators.required, Validators.minLength(3)]],
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(5)]],
+      confirmPassword: ["", [Validators.required, Validators.minLength(5)]],
+    },
+    { validators: passwordMatchValidator, updateOn: "blur" }
+  );
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit() {}
+
+  get firstName() {
+    return this.registerForm.get("firstName");
   }
 
+  get lastName() {
+    return this.registerForm.get("lastName");
+  }
+
+  get email() {
+    return this.registerForm.get("email");
+  }
+
+  get password() {
+    return this.registerForm.get("password");
+  }
+
+  get confirmPassword() {
+    return this.registerForm.get("confirmPassword");
+  }
 }
