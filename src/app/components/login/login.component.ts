4d0a2f3dc93d95580f7297118eb2585c08d80a49
Akhil Naidu <akhiln1994@gmail.com>
2020-10-28 12:35:27 +0530

    added validation for login screen

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private router: Router) { }
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+  }, { updateOn: 'blur' })
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+
+  }
+
+  setNullErrors(formControl: AbstractControl) {
+    formControl.setErrors(null);
+  }
+
+  get email() {
+    return this.loginForm.get("email");
+  }
+
+  get password() {
+    return this.loginForm.get("password");
   }
 
 }
